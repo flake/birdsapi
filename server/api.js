c7fec8d73cfb0c517f1d7548a6894c0e9188208c
Rajasekhar <rajasekahr@ynpole.com>
2016-07-05 18:59:18 +0530

    birds api assignment

--- server/api.js
+++ server/api.js
@@ -1,17 +1,27 @@
 Api.addCollection(Birds);
 
-// Api.addRoute('/birds/:_id',{
-//   get: function(){
-//     var id = this.urlParams._id;
-//     return Birds.findOne(id);
+// Api.addRoute('/birds/:_id', {authRequired: false},{
+//   get: {
+//     action: function(){
+//       var id = this.urlParams._id;
+//       return Birds.findOne(id);
+//     }
 //   }
 // });
 //
-// Api.addRoute('/birds', {
-//   get: function(){
-//     return Birds.find();
-//   },
-//   post: function(){
+// Api.addRoute('/birds', {authRequired: false}, {
+//   get: {
+//     action: function(){
+//       var birds = Birds.find();
+//       if(!birds)
+//         return {status: "404 Not found"};
 //
+//       return birds;
+//     }
+//   },
+//   post: {
+//     action: function(){
+//       return this.request;
+//     }
 //   }
 // });
--- server/methods.js
+++ server/methods.js
@@ -2,5 +2,14 @@ Meteor.methods({
   addBird: function(bird){
     var birdId = Birds.insert(bird);
     return Birds.findOne(birdId);
+  },
+  allBirds: function(){
+    return Birds.find();
+  },
+  getBird: function(birdId){
+    return Birds.findOne(birdId);
+  },
+  deleteBird: function(birdId){
+    return Birds.remove(birdId);
   }
 });
